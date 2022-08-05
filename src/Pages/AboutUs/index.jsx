13033a463d32b6c3dcd4f813d7bb6bca58da0da7
nlok5923 <nlokhande5923@gmail.com>
2022-08-05 12:46:09 +0530

    added about page

---
+++ src/Pages/AboutUs/index.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import './AboutUs.scss'
+
+const AboutUs = () => {
+    return (
+        <div>
+        <div className="under-work">
+            Page Under work 
+        </div>
+        </div>
+    )
+}
+
+export default AboutUs;
