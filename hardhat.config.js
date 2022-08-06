40105b0d5521bc4e3de9065f287e640ff7ad99f3
nlok5923 <nlokhande5923@gmail.com>
2022-08-06 09:40:31 +0530

    hiding + nothiding variables

--- hardhat.config.js
+++ hardhat.config.js
@@ -18,11 +18,11 @@ module.exports = {
   networks: {
      hardhat: {},
      goerli: {
-        url: "https://eth-goerli.alchemyapi.io/v2/V5p1PckEwUqIq5s5rA2zvwRKH0V9Hslr",
-        accounts: ["e3f4cd385c1834aba7227db3b6468cf1805a2831463042781c529a1beacf9f25"]
+        url: process.env.API_URL,
+        accounts: [process.env.PRIVATE_KEY]
      }
   },
   etherscan: {
-    apiKey: "XIZ6C66146CC6QSZYVFAM5WMTH9EHWDWEI"
+    apiKey: process.env.ETH_KEY
   }
 };
